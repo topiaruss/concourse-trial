2a57bffeab598dc6381511c900478888bb466d00
Russ Ferriday <russ.ferriday@swga.com>
2017-11-16 22:54:32 +0000

    first successful pipeline

--- a/run.sh
+++ a/run.sh
@@ -2,26 +2,28 @@
 
 set -ex
 
-realpath() {
-  [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
-}
+# realpath() {
+#   [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+# }
 
-usage() {
-  echo "USAGE: run.sh path/to/credentials.yml"
-  exit 1
-}
+# usage() {
+#   echo "USAGE: run.sh path/to/credentials.yml"
+#   exit 1
+# }
 
-if [ -z "${stub}" ]; then
-  stub="~/.quay_io.yaml"
-fi
-stub=$(realpath $stub)
-if [[ ! -f ${stub} ]]; then
-  usage
-fi
+# if [ -z "${stub}" ]; then
+#   stub="~/.concourse_credentials.yaml"
+# fi
+# stub=$(realpath $stub)
+# if [[ ! -f ${stub} ]]; then
+#   usage
+# fi
 
-fly_target=
+stub=~/.concourse_credentials.yaml
+
+fly_target=lite
 #pushd $DIR
-  fly sp -t ${fly_target} configure -c pipeline.yml -p main --load-vars-from ${stub} -n
+  fly -t ${fly_target} sp configure -c pipeline.yaml -p main --load-vars-from ${stub} -n
   fly -t ${fly_target} unpause-pipeline --pipeline main
   fly -t ${fly_target} trigger-job -j main/concourse-trial-publish
   fly -t ${fly_target} watch -j main/concourse-trial-publish
