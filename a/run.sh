d8be2d8c4ee282ea253d88befec9ac4343d6c496
Russ Ferriday <russ.ferriday@swga.com>
2017-11-17 02:03:58 +0000

    try rename

--- a/run.sh
+++ a/run.sh
@@ -23,8 +23,8 @@ stub=~/.concourse_credentials.yaml
 
 fly_target=lite
 #pushd $DIR
-  fly -t ${fly_target} sp configure -c pipeline.yaml -p main --load-vars-from ${stub} -n
-  fly -t ${fly_target} unpause-pipeline --pipeline main
-  fly -t ${fly_target} trigger-job -j main/concourse-trial-publish-a
-  fly -t ${fly_target} watch -j main/concourse-trial-publish-a
+  fly -t ${fly_target} sp configure -c pipeline.yaml -p maina --load-vars-from ${stub} -n
+  fly -t ${fly_target} unpause-pipeline --pipeline maina
+  fly -t ${fly_target} trigger-job -j maina/concourse-trial-publish-a
+  fly -t ${fly_target} watch -j maina/concourse-trial-publish-a
 #popd
