b848d16cc76fe67891c8c8f9c3f77e2639fed9b2
Russ Ferriday <russ.ferriday@swga.com>
2017-11-17 02:27:18 +0000

    shorten sleep

--- a/component.py
+++ a/component.py
@@ -8,6 +8,6 @@ while 1:
     for i in range(10):
         mess = 'world %s' % datetime.datetime.now()
         p.produce('testloop', key='hello', value=mess)
-    time.sleep(1000)
+    time.sleep(1)
     print('flush ' + mess)
     p.flush()
