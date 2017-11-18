5a43ae007399e7f971e84e581bb42131da7a2d0e
Russ Ferriday <russ.ferriday@swga.com>
2017-11-18 21:35:27 +0000

    tidy

--- run.sh
+++ run.sh
@@ -2,29 +2,19 @@
 
 set -ex
 
-# realpath() {
-#   [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
-# }
+creds=~/.concourse_credentials.yaml
 
-# usage() {
-#   echo "USAGE: run.sh path/to/credentials.yml"
-#   exit 1
-# }
-
-# if [ -z "${stub}" ]; then
-#   stub="~/.concourse_credentials.yaml"
-# fi
-# stub=$(realpath $stub)
-# if [[ ! -f ${stub} ]]; then
-#   usage
-# fi
-
-stub=~/.concourse_credentials.yaml
+# The build system - often forwarded like:
+# export POD_NAME=$(kubectl get pods --namespace default -l "app=wise-possum-web" \
+#    -o jsonpath="{.items[0].metadata.name}")
+# kubectl port-forward --namespace default $POD_NAME 8080:8080
 
 fly_target=lite
-#pushd $DIR
-  fly -t ${fly_target} sp configure -c pipeline.yaml -p conc-trial-combo --load-vars-from ${stub} -n
-  fly -t ${fly_target} unpause-pipeline --pipeline conc-trial-combo
-  # fly -t ${fly_target} trigger-job -j conc-trial-combo/concourse-trial-publish-combo
-  fly -t ${fly_target} watch -j conc-trial-combo/concourse-trial-publish-combo
-#popd
+fly -t lite login -c http://localhost:8080
+
+fly -t ${fly_target} sp configure -c pipeline.yaml -p conc-trial-combo --load-vars-from ${creds} -n
+fly -t ${fly_target} unpause-pipeline --pipeline conc-trial-combo
+
+# if you want to automatically trigger and watch
+#fly -t ${fly_target} trigger-job -j conc-trial-combo/concourse-trial-publish-combo
+#fly -t ${fly_target} watch -j conc-trial-combo/concourse-trial-publish-combo
