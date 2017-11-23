78ed9cd056ba646c1819f6a61bb5a67436f3ad08
Russ Ferriday <russf@topia.com>
2017-11-23 03:01:56 +0000

    Switch to msgflow abstraction.

--- a/component.py
+++ a/component.py
@@ -2,14 +2,17 @@ from confluent_kafka import Producer
 import datetime
 import multiprocessing
 import time
+from msgflow.api import Msgflow
 
 print("starting A - cpu_count: %s" % multiprocessing.cpu_count())
 
-p = Producer({'bootstrap.servers': 'winsome-hare-kafka:9092'})
+flow = Msgflow({'bootstrap.servers':
+                'winsome-hare-kafka.default.svc.cluster.local:9092'})
 while 1:
     for i in range(10):
-        mess = 'world %s' % datetime.datetime.now()
-        p.produce('testloop', key='hello', value=mess)
+        msg = dict(hello='world at %s' % datetime.datetime.now())
+        flow.put('testloop2', msg)
+    print('flush %s' % msg)
+    flow.flush()
     time.sleep(1)
-    print('flush ' + mess)
-    p.flush()
+
--- b/component.py
+++ b/component.py
@@ -1,35 +1,20 @@
 from confluent_kafka import Consumer, KafkaError
 import multiprocessing
-
-settings = {
-    'bootstrap.servers': 'winsome-hare-kafka:9092',
-    'group.id': 'mygroup',
-    'client.id': 'client-1',
-    'enable.auto.commit': True,
-    'session.timeout.ms': 6000,
-    'default.topic.config': {'auto.offset.reset': 'smallest'}
-}
+from msgflow.api import Msgflow
 
 print("starting B - cpu_count: %s" % multiprocessing.cpu_count())
 
-c = Consumer(settings)
-c.subscribe(['testloop'])
-
+flow = Msgflow({'bootstrap.servers':
+                'winsome-hare-kafka.default.svc.cluster.local:9092'})
+consumer = flow.get_consumer('testloop2')
 try:
     while True:
-        msg = c.poll(0.1)
+        msg = consumer.get(timeout=0.1)
         if msg is None:
             continue
-        elif not msg.error():
-            print('Received message: {0}'.format(msg))
-        elif msg.error().code() == KafkaError._PARTITION_EOF:
-            print('End of partition reached {0}/{1}'
-                  .format(msg.topic(), msg.partition()))
         else:
-            print('Error occured: {0}'.format(msg.error().str()))
-
+            print('Received message: {0}'.format(msg))
 except KeyboardInterrupt:
     pass
-
 finally:
-    c.close()
+    consumer.close()
