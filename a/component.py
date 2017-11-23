8cc0f92086f8ce9c42e6ac1c37b4ff4376e2bed5
Russ Ferriday <russf@topia.com>
2017-11-23 23:31:46 +0000

    faster

--- a/component.py
+++ a/component.py
@@ -8,10 +8,11 @@ print("starting A - cpu_count: %s" % multiprocessing.cpu_count())
 flow = Msgflow({'bootstrap.servers':
                 'winsome-hare-kafka.default.svc.cluster.local:9092'})
 while 1:
-    for bits in range(17):
+    for bits in range(19):
         start_level_at = time.time()
-        end_at = start_level_at + 30.0
+        end_at = start_level_at + 20.0
         wait = 1.0 / (1 << bits)
+        print(bits, wait)
         while time.time() < end_at:
             msg = dict(hello='world %s at %s' %
                       (wait, datetime.datetime.now()))
