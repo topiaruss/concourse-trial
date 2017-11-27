b0ea89cbf3b86b1c756bdc75c442a8087ddaeaab
Russ Ferriday <russf@topia.com>
2017-11-27 21:51:12 +0000

    component moved to msgflow package.

--- run.sh
+++ run.sh
@@ -3,15 +3,9 @@
 set -ex
 
 creds=~/.concourse_credentials.yaml
-
-# The build system - often forwarded like:
-# export POD_NAME=$(kubectl get pods --namespace default -l "app=wise-possum-web" \
-#    -o jsonpath="{.items[0].metadata.name}")
-# kubectl port-forward --namespace default $POD_NAME 8080:8080
-
+CONCOURSE_HOST=${CONCOURSE_HOST-concourse}
 fly_target=lite
-fly -t lite login -c http://localhost:8080
-
+#fly -t ${fly_target} login -c http://${CONCOURSE_HOST}:8080
 fly -t ${fly_target} sp configure -c pipeline.yaml -p conc-trial-combo --load-vars-from ${creds} -n
 fly -t ${fly_target} unpause-pipeline --pipeline conc-trial-combo
 
