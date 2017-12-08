41265649ebb3dc213cbe3ac721d616bea3f529a3
Russ Ferriday <russf@topia.com>
2017-12-08 22:38:20 +0000

    replace exception in sub2

--- alpha/component.py
+++ alpha/component.py
@@ -31,12 +31,12 @@ class Alpha(Component):
     @zipkin_client_span(service_name='Alpha', span_name='sub1.sub2')
     def sub1sub2(self):
         time.sleep(0.02)
-        raise ValueError('Dummy exception')
-
+        # when exception here - action ends with sub2. Never enqueue beta_topic. Never reach sub2.
 
     @zipkin_client_span(service_name='Alpha', span_name='sub2')
     def sub2(self):
         time.sleep(0.03)
+        raise ValueError('Dummy exception')
 
 
 a = Alpha('alpha_topic')
