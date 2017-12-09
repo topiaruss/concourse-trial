4e0f7d029a7c1130ce3f4f68d500f203fea62315
Russ Ferriday <russf@topia.com>
2017-12-09 23:52:35 +0000

    fix missing context

--- beta/component.py
+++ beta/component.py
@@ -4,7 +4,7 @@ import time
 import datetime
 
 from msgflow.component import Component, Msgflow
-from py_zipkin.zipkin import zipkin_client_span
+from py_zipkin.zipkin import zipkin_span
 
 print("starting Beta - cpu_count: %s" % multiprocessing.cpu_count())
 
@@ -14,13 +14,14 @@ class Beta(Component):
         super().__init__(topic, pre_drain=True)
 
     def process_one(self, zipkin_context, data):
+        self.zipkin_context = zipkin_context
         self.sub1()
         self.enqueue_item(zipkin_context, 'gamma_topic', dict(gamma=3))
         self.enqueue_item(zipkin_context, 'gamma_topic', dict(gamma=4))
         time.sleep(0.02)
         self.sub1()
 
-    @zipkin_client_span(service_name='Beta', span_name='sub1')
+    @zipkin_span(service_name='Beta', span_name='sub1')
     def sub1(self):
         self.zipkin_context.update_binary_annotations(dict(
             autumn='in the shade'))
--- gamma/component.py
+++ gamma/component.py
@@ -4,7 +4,7 @@ import time
 import datetime
 
 from msgflow.component import Component, Msgflow
-from py_zipkin.zipkin import zipkin_client_span
+from py_zipkin.zipkin import zipkin_span
 
 print("starting Gamma - cpu_count: %s" % multiprocessing.cpu_count())
 
@@ -14,13 +14,14 @@ class Gamma(Component):
         super().__init__(topic, pre_drain=True)
 
     def process_one(self, zipkin_context, data):
+        self.zipkin_context = zipkin_context
         self.sub1()
 
-    @zipkin_client_span(service_name='Gamma', span_name='sub1')
+    @zipkin_span(service_name='Gamma', span_name='sub1')
     def sub1(self):
         self.zipkin_context.update_binary_annotations(dict(
             winter='in the rain',
-            zdict=dict(a=1,b=2,c='see')
+            zdict=dict(a=1, b=2, c='see')
         ))
         time.sleep(0.02)
 
