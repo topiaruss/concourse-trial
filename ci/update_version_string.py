60138b51c1095f62ff57841e8d2d734fe249be00
Russ Ferriday <russf@topia.com>
2017-12-11 13:54:37 +0000

    put script into repo for execution

--- ci/update_version_string.py
+++ ci/update_version_string.py
@@ -11,6 +11,6 @@ from setuptools_scm import get_version
 dirs = [d.split('/')[0] for d in glob.glob('*/Dockerfile')]
 for d in dirs:
     w_to = '%s/version.py' % d
-    version = get_version(root=".",
+    version = get_version(root="..",
                           relative_to=__file__,
                           write_to=w_to)
