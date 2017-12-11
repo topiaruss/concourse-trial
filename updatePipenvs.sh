feb1c1650330002fbb85c7feaa8e1fd3d6453370
Russ Ferriday <russf@topia.com>
2017-12-11 20:43:29 +0000

    Add ci file

--- updatePipenvs.sh
+++ updatePipenvs.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 ( pipenv update) &
-( cd a; pipenv update > a.update.log )&
-( cd b; pipenv update > b.update.log )&
 ( cd alpha; pipenv update > alpha.update.log)&
 ( cd beta; pipenv update > beta.update.log )&
 ( cd gamma; pipenv update > gamma.update.log )&
